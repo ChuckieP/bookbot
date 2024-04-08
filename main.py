f6c9db4d83a50174885615a712f36a520fecc5a1
ChuckieP <chuckporcelli@gmail.com>
2024-04-08 17:03:05 -0500

    printing book contents to console

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def count_words():
+    pass
+
+
+main()
