1cb7d4a90a128c7f02fefdd717a3dae65e031979
ChuckieP <chuckporcelli@gmail.com>
2024-04-08 17:41:12 -0500

    printing char count to console

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     print(f"{word_count} words found in the document")
+    char_count = count_chars(text)
+    print(char_count)
 
 
 def get_book_text(path):
@@ -13,6 +15,17 @@ def get_book_text(path):
 def count_words(str_input):
     words = str_input.split()
     return len(words)
+
+
+def count_chars(str_input):
+    chars = {}
+    for c in str_input:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 main()
